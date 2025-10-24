72442ac9da77a277d259f63db41c3ae77a0e34a2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-10-24 15:32:50 -0700

    Add description and homepage for vsekai-godot; remove livecheck stanza to fix installation errors

--- Casks/vsekai-godot.rb
+++ Casks/vsekai-godot.rb
@@ -1,22 +1,20 @@
 # Documentation: https://docs.brew.sh/Cask-Cookbook
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 cask "vsekai-godot" do
   version "latest.v-sekai-editor-258"
   sha256 "d16dc2b57f363a7db552f20502714c30b226773446d6014a6cc01a568bfa7762"
 
   url "https://github.com/V-Sekai/world-godot/releases/download/#{version}/v-sekai-godot-macos.zip"
   name "vsekai-godot"
-  desc ""
-  homepage ""
+  desc "V-Sekai Godot Editor"
+  homepage "https://github.com/V-Sekai/world-godot"
 
-  # Documentation: https://docs.brew.sh/Brew-Livecheck
-  livecheck do
-    url "https://github.com/V-Sekai/world-godot/releases"
-    strategy :github_latest
-  end
+  # Removed livecheck stanza to resolve persistent installation errors.
+  # This can be re-added later if needed with careful testing.
 
-  app ""
+  app "vsekai-godot.app"
 
   # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
   zap trash: ""
