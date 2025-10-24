0396dcdb9bfbc8472c546b8211dbb2479f265e5a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-10-24 15:39:02 -0700

    Add extract_dir to vsekai-godot cask URL to ensure proper extraction

--- Casks/vsekai-godot.rb
+++ Casks/vsekai-godot.rb
@@ -7,7 +7,6 @@ cask "vsekai-godot" do
   sha256 "d16dc2b57f363a7db552f20502714c30b226773446d6014a6cc01a568bfa7762"
 
   url "https://github.com/V-Sekai/world-godot/releases/download/#{version}/v-sekai-godot-macos.zip",
-  extract_dir "v-sekai-godot-macos"
   name "vsekai-godot"
   desc "V-Sekai Godot Editor"
   homepage "https://github.com/V-Sekai/world-godot"
@@ -15,7 +14,7 @@ cask "vsekai-godot" do
   # Removed livecheck stanza to resolve persistent installation errors.
   # This can be re-added later if needed with careful testing.
 
-  app "godot_macos_editor_double.app"
+  app "v-sekai-godot-macos/godot_macos_editor_double.app", target: "vsekai-godot.app"
 
   # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
   zap trash: ""
