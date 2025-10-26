96a729c059706dcf4687a36b62d4786ea7bf541b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-10-26 10:00:21 -0700

    Update Desync formula to correct revision hash

--- Formula/desync.rb
+++ Formula/desync.rb
@@ -3,7 +3,7 @@ class Desync < Formula
   homepage "https://github.com/folbricht/desync"
   url "https://github.com/folbricht/desync.git",
       tag:      "v0.9.6",
-      revision: "009540f65c1d26ae3c7dab40e45df0dbc2a71011"
+      revision: "0aef76def97e1aa148d58b8a831f7c89b41ba9c4"
 
   depends_on "go" => :build
 
