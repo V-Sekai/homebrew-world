6d880d248378b0ed54e6074d58eea58eabdcd2c7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-01-05 12:12:38 -0800

    Update vsekai-godot to latest.v-sekai-editor-269

--- Casks/vsekai-godot-dev.rb
+++ Casks/vsekai-godot-dev.rb
@@ -1,6 +1,6 @@
 cask "vsekai-godot-dev" do
-  version "latest.v-sekai-editor-258"
-  sha256 "d16dc2b57f363a7db552f20502714c30b226773446d6014a6cc01a568bfa7762"
+  version "latest.v-sekai-editor-269"
+  sha256 "f2ad16f1e63902d769bb6ac110d8dba9d131937610944aad393f1860d9e1e0c9"
 
   url "https://github.com/V-Sekai/world-godot/releases/download/#{version}/v-sekai-godot-macos.zip"
 
