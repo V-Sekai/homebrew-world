1952033e0e7a3c9d9713a6539636bbabafbeaac7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-01-07 13:32:20 -0800

    Update vsekai-godot to latest.v-sekai-editor-270 and fix template checksums

--- Casks/vsekai-godot.rb
+++ Casks/vsekai-godot.rb
@@ -1,10 +1,9 @@
 cask "vsekai-godot" do
-  version "latest.v-sekai-editor-269.3"
-  sha256 "f2ad16f1e63902d769bb6ac110d8dba9d131937610944aad393f1860d9e1e0c9"
+  version "latest.v-sekai-editor-270.3"
+  sha256 "ba27caf86eef62ee7b27cda55c7d1fefdd942fdd531cf6a88fa90e984a0d3ada"
 
-  release_version = "latest.v-sekai-editor-269"
+  release_version = "latest.v-sekai-editor-270"
   url "https://github.com/V-Sekai/world-godot/releases/download/#{release_version}/v-sekai-godot-macos.zip"
-
   name "vsekai-godot"
   desc "V-Sekai Godot Editor"
   homepage "https://github.com/V-Sekai/world-godot"
@@ -20,24 +19,24 @@ cask "vsekai-godot" do
 
     # Download template files
     templates_url = "https://github.com/V-Sekai/world-godot/releases/download/#{release_version}/v-sekai-godot-templates.zip.001"
-    templates_sha256 = "7044f0035d6de1ae48a2b9ee9e764c7f72deef837612e9dfd35a8f4203be0084"
+    templates_sha256 = "38fa87b2f7c86585e6ef9f48fb56941890015164a3ae30706b7c734fb4521368"
     templates_file_001 = "#{temp_dir}/v-sekai-godot-templates.zip.001"
-    
+
     system_command "curl", args: ["-L", "-o", templates_file_001, templates_url]
     system_command "shasum", args: ["-a", "256", "-c", "-"], input: "#{templates_sha256}  #{templates_file_001}"
 
     # Download symbols template files (split zip)
     symbols_url_001 = "https://github.com/V-Sekai/world-godot/releases/download/#{release_version}/v-sekai-godot-templates-symbols.zip.001"
-    symbols_sha256_001 = "48ce46e4b16b64d26730854e3a2ead23f9c8ac9e5948d3b7d5c86ad69bc8610a"
+    symbols_sha256_001 = "bf190b4fad0dc3300f3aa53ec1a154ee62cdd88c29e5e42ea62240ec97f92afa"
     symbols_file_001 = "#{temp_dir}/v-sekai-godot-templates-symbols.zip.001"
-    
+
     symbols_url_002 = "https://github.com/V-Sekai/world-godot/releases/download/#{release_version}/v-sekai-godot-templates-symbols.zip.002"
-    symbols_sha256_002 = "3e21fd591e2e9d8a07edcea6504b4f377f91aa212cb72955b3f2ac37454b75a9"
+    symbols_sha256_002 = "0ae720ee02ec42bf7484e408a938a366c1821da8fab51bce83bd0ad6b0a6f7c2"
     symbols_file_002 = "#{temp_dir}/v-sekai-godot-templates-symbols.zip.002"
-    
+
     system_command "curl", args: ["-L", "-o", symbols_file_001, symbols_url_001]
     system_command "shasum", args: ["-a", "256", "-c", "-"], input: "#{symbols_sha256_001}  #{symbols_file_001}"
-    
+
     system_command "curl", args: ["-L", "-o", symbols_file_002, symbols_url_002]
     system_command "shasum", args: ["-a", "256", "-c", "-"], input: "#{symbols_sha256_002}  #{symbols_file_002}"
 
@@ -48,46 +47,45 @@ cask "vsekai-godot" do
       templates_combined = "#{temp_dir}/v-sekai-godot-templates.zip"
       # Templates zip might be a single file or already complete
       FileUtils.cp templates_file_001, templates_combined
-      
+
       symbols_combined = "#{temp_dir}/v-sekai-godot-templates-symbols.zip"
       # Combine split symbols zip files using cat
       system_command "sh", args: ["-c", "cat '#{symbols_file_001}' '#{symbols_file_002}' > '#{symbols_combined}'"]
-      
+
       # Extract to temp location first
       extract_temp = "#{temp_dir}/extract"
       FileUtils.mkdir_p extract_temp
       system_command "unzip", args: ["-o", templates_combined, "-d", extract_temp]
       system_command "unzip", args: ["-o", symbols_combined, "-d", extract_temp]
-      
+
       # Read version from version.txt in extracted templates
       version_file = Dir.glob("#{extract_temp}/**/version.txt").first
       template_version = nil
       if version_file && File.exist?(version_file)
         template_version = File.read(version_file).strip
         # Verify version.txt was read correctly and is not empty
-        if template_version.nil? || template_version.empty?
+        if template_version.blank?
           template_version = nil
         else
           # Use the version from version.txt (e.g., "4.6.beta.double")
           # This is the actual Godot version, not the release tag
         end
       end
-      
+
       # If version.txt not found or invalid, we can't proceed without knowing the version
-      if template_version.nil?
-        raise "Could not determine template version from version.txt"
-      end
-      
+      raise "Could not determine template version from version.txt" if template_version.nil?
+
       templates_dir = "#{base_templates_dir}/#{template_version}"
       FileUtils.mkdir_p templates_dir
-      
+
       # Move all files directly to templates_dir (flatten any subdirectories)
       Dir.glob("#{extract_temp}/**/*").each do |file|
         next if File.directory?(file)
+
         target_file = "#{templates_dir}/#{File.basename(file)}"
         FileUtils.mv file, target_file, force: true
       end
-      
+
       # Verify version.txt exists in templates_dir
       version_txt_path = "#{templates_dir}/version.txt"
       unless File.exist?(version_txt_path)
@@ -95,9 +93,9 @@ cask "vsekai-godot" do
         File.write(version_txt_path, template_version)
       end
     end
-    
+
     # Clean up temporary files
-    FileUtils.rm_rf temp_dir
+    FileUtils.rm_r(temp_dir)
   end
 
   zap trash: ""
